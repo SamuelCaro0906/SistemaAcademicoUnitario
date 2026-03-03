897532614c0d040e63cd82ec658b38cbe7109b84
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-03-03 08:32:58 -0500

    FEat: Crear la clase nota con atributos

---
+++ src/sistemaacademicounitario/Nota.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademicounitario;
+
+/**
+ *
+ * @author Csamu
+ */
+public class Nota {
+     private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;   
+}
