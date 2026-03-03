ee50129c8cd6f84483a46deea9dbd6ebb964e742
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-03-03 06:25:40 -0500

    Feat: Crear la Clase ASignatura con atributos

---
+++ src/sistemaacademicounitario/Asignatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademicounitario;
+
+/**
+ *
+ * @author Csamu
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+}
