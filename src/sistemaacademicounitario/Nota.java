9258b1dcc910692569a3be966d590dc40af9e67a
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-03-03 09:43:52 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/sistemaacademicounitario/Nota.java
+++ src/sistemaacademicounitario/Nota.java
@@ -13,4 +13,36 @@ public class Nota {
     private Asignatura asignatura;
     private double valor;
     private String periodo;   
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
