3856e688543e1a6a9932d3af2fccbfde83fded5d
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-03-03 06:36:26 -0500

    Feat: agregar encapsulamiento a clase asignatura

--- src/sistemaacademicounitario/Asignatura.java
+++ src/sistemaacademicounitario/Asignatura.java
@@ -13,4 +13,37 @@ public class Asignatura {
     private String nombre;
     private int creditos;
     private String docente;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+    
 }
